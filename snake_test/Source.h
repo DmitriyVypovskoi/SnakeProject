b31f0e51d533197e34105e229868d430b323c71a
Matvey <69459474+FlameTigerFlash@users.noreply.github.com>
2022-06-18 21:51:22 +0300

    Update Source.h

--- snake_test/Source.h
+++ snake_test/Source.h
@@ -1,5 +1,75 @@
-int get_random_empty_cell();
-auto apple_add();
-auto clear_field();
-auto increaseSnake();
-auto movement();
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <cstdlib>
+#include <vector>
+#include <string>
+
+const int field_cell_type_none = 0;
+const int field_cell_type_apple = -1;
+const int field_cell_type_wall = -2;
+const int snake_direction_up = 0;
+const int snake_direction_right = 1;
+const int snake_direction_down = 2;
+const int snake_direction_left = 3;
+const int field_size_x = 35;
+const int field_size_y = 25;
+const int cell_size = 32;
+const int window_width = field_size_x * cell_size;
+const int window_height = field_size_y * cell_size;
+
+struct Game
+{
+    int field[field_size_x][field_size_y];
+    int snake_position_x = field_size_x / 2;
+    int snake_position_y = field_size_y / 2;
+    int snake_length = 4;
+    int snake_direction = snake_direction_right;
+    int score = 0;
+
+    bool game_paused = false;
+    bool game_over = false;
+
+    sf::Texture snake_picture;
+    sf::Sprite snake;
+
+    sf::Texture none_picture;
+    sf::Sprite none;
+
+    sf::Texture apple_picture;
+    sf::Sprite apple;
+
+    sf::Texture wall_picture;
+    sf::Sprite wall;
+
+    sf::Texture snake_head_picture;
+    sf::Sprite snake_head;
+
+    sf::SoundBuffer sb_ate_apple;
+    sf::Sound sound_ate_apple;
+
+    sf::SoundBuffer sb_snake_kill_yourself;
+    sf::Sound sound_snake_kill_yourself;
+
+    sf::SoundBuffer sb_pause_game;
+    sf::Sound sound_pause_game;
+
+    sf::SoundBuffer sb_snake_dead_from_wall;
+    sf::Sound sound_snake_dead_from_wall;
+
+    sf::SoundBuffer sb_score10;
+    sf::Sound sound_score10;
+
+    sf::Font font_score;
+    sf::Text text_score;
+
+    sf::Font font_game_over;
+    sf::Text text_game_over;
+
+    void init_game();
+    int get_random_empty_cell();
+    void apple_add();
+    void clear_field();
+    void draw_field();
+    void increaseSnake();
+    void movement();
+};
