ac8de99c04c45e4a0e09d55912eee1655c34620b
DmitriyVypovskoi <davypovskoy@miem.hse.ru>
2022-06-18 14:54:21 +0300

    v8

---
+++ cmake-build-debug/doctest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/dvypo/CLionProjects/snakeproject/doctest
+# Build directory: C:/Users/dvypo/CLionProjects/snakeproject/cmake-build-debug/doctest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
