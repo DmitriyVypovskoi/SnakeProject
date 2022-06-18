9a5977781dd67cf9c7f39a8b3de5f1c01d731bb8
Matvey <69459474+FlameTigerFlash@users.noreply.github.com>
2022-06-18 21:50:38 +0300

    Update Source.cpp

--- snake_test/Source.cpp
+++ snake_test/Source.cpp
@@ -1,22 +1,61 @@
-#include <string>
 #include "Source.h"
 
-const int field_cell_type_none = 0;
-const int field_cell_type_apple = -1;
-const int field_cell_type_wall = -2;
-const int snake_direction_up = 0;
-const int snake_direction_right = 1;
-const int snake_direction_down = 2;
-const int snake_direction_left = 3;
+void Game::init_game()
+{
+    std::srand(time(NULL));
 
-const int field_size_x = 35;
-const int field_size_y = 25;
-const int cell_size = 32;
+    snake_picture.loadFromFile("resources/images/snake.png");
+    snake.setTexture(snake_picture);
 
-using namespace std;
+    none_picture.loadFromFile("resources/images/none.png");
+    none.setTexture(none_picture);
 
-// Игровая функция по выбору случайной пустой клетки
-int get_random_empty_cell(int field[field_size_x][field_size_y])
+    apple_picture.loadFromFile("resources/images/apple.png");
+    apple.setTexture(apple_picture);
+
+    wall_picture.loadFromFile("resources/images/wall.png");
+    wall.setTexture(wall_picture);
+
+    snake_head_picture.loadFromFile("resources/images/head.png");
+    snake_head.setTexture(snake_head_picture);
+
+    sb_ate_apple.loadFromFile("resources/sounds/get_apple2.wav");
+    sound_ate_apple.setBuffer(sb_ate_apple);
+    sound_ate_apple.setVolume(40);
+
+    sb_snake_kill_yourself.loadFromFile("resources/sounds/died.wav");
+    sound_snake_kill_yourself.setBuffer(sb_snake_kill_yourself);
+    sound_snake_kill_yourself.setVolume(50);
+
+    sb_snake_dead_from_wall.loadFromFile("resources/sounds/dead.wav");
+    sound_snake_dead_from_wall.setBuffer(sb_snake_dead_from_wall);
+    sound_snake_dead_from_wall.setVolume(70);
+
+    sb_pause_game.loadFromFile("resources/sounds/pause_game.wav");
+    sound_pause_game.setBuffer(sb_pause_game);
+    sound_pause_game.setVolume(20);
+
+    sb_score10.loadFromFile("resources/sounds/score10.wav");
+    sound_score10.setBuffer(sb_score10);
+    sound_score10.setVolume(45);
+
+    font_score.loadFromFile("resources/fonts/BigFat.otf");
+    text_score.setFont(font_score);
+
+    font_game_over.loadFromFile("resources/fonts/BigOld.ttf");
+    text_game_over.setFont(font_game_over);
+
+    text_game_over.setString("GAME OVER");
+    text_game_over.setCharacterSize(86);
+    text_game_over.setFillColor(sf::Color::Blue);
+    text_game_over.setPosition((window_width - text_game_over.getLocalBounds().width) / 2, (window_height - text_game_over.getLocalBounds().height) / 2 - 30);
+}
+
+/**
+ *
+ * @return
+ */
+int Game::get_random_empty_cell()
 {
     int empty_cell_count = 0;
     for (int j = 0; j < field_size_y; j++) {
@@ -40,18 +79,23 @@ int get_random_empty_cell(int field[field_size_x][field_size_y])
     }
     return -1;
 }
-
-//Игровая функция по добавлению яблока в случайную клетку
-auto apple_add(int field[field_size_x][field_size_y])
+/**
+ * @param applePosition
+ * @return
+ */
+void Game::apple_add()
 {
-    int applePosition = get_random_empty_cell(field);
+    int applePosition = get_random_empty_cell();
     if (applePosition != -1) {
         field[applePosition / field_size_x][applePosition % field_size_x] = field_cell_type_apple;
     }
 }
 
-//Игровая функция по очищению всего игрового поля
-auto clear_field(int field[field_size_x][field_size_y], int snake_length, int snake_position_x, int snake_position_y)
+/**
+ * Method clear_field
+ * @return
+ */
+void Game::clear_field()
 {
     for (int i = 0; i < field_size_x; i++) {
         for (int j = 0; j < field_size_y; j++) {
@@ -73,11 +117,85 @@ auto clear_field(int field[field_size_x][field_size_y], int snake_length, int sn
             field[field_size_x - 1][i] = field_cell_type_wall;
         }
     }
-    apple_add(field);
+    apple_add();
 }
 
-//Игровая функция по увеличению длины змейки
-auto increaseSnake(int field[field_size_x][field_size_y])
+/**
+ * Method draw_field - draw cell in the window
+ * sf::Color - sets the text color
+ * snake_head.getLocalBounds() - retrieves text dimensions height or width
+ * std::to_string - turns a value into a string
+ * @param window a window a render on
+ */
+
+void Game::draw_field(sf::RenderWindow& window)
+{
+
+    for (int i = 0; i < field_size_x; i++) {
+        for (int j = 0; j < field_size_y; j++) {
+            switch (field[i][j]) {
+                case field_cell_type_none:
+                    none.setPosition(float(i * cell_size), float(j * cell_size));
+                    window.draw(none);
+                    break;
+                case field_cell_type_apple:
+                    apple.setPosition(float(i * cell_size), float(j * cell_size));
+                    window.draw(apple);
+                    break;
+                case field_cell_type_wall:
+                    wall.setPosition(float(i * cell_size), float(j * cell_size));
+                    window.draw(wall);
+                    break;
+                default:
+                    if (field[i][j] == snake_length) {
+                        float offset_x = snake_head.getLocalBounds().width / 2;
+                        float offset_y = snake_head.getLocalBounds().height / 2;
+                        snake_head.setPosition(float(i * cell_size + offset_x), float(j * cell_size) + offset_y);
+                        snake_head.setOrigin(offset_x, offset_y);
+                        switch (snake_direction) {
+                            case snake_direction_up:
+                                snake_head.setRotation(90);
+                                break;
+                            case snake_direction_right:
+                                snake_head.setRotation(-180);
+                                break;
+                            case snake_direction_left:
+                                snake_head.setRotation(0);
+                                break;
+                            case snake_direction_down:
+                                snake_head.setRotation(-90);
+                                break;
+                        }
+                        window.draw(snake_head);
+                    }
+                    else {
+                        snake.setPosition(float(i * cell_size), float(j * cell_size));
+                        window.draw(snake);
+                    }
+            }
+        }
+    }
+    // Draw score_bar//
+    text_score.setString("Score: " + std::to_string(score));
+    text_score.setCharacterSize(28);
+    if (score >= 20) {
+        text_score.setFillColor(sf::Color::Blue);
+        text_score.setPosition(window_width / 2 - 65, 0);
+        window.draw(text_score);
+    }
+    else {
+        text_score.setFillColor(sf::Color::Black);
+        text_score.setPosition(window_width / 2 - 65, 0);
+        window.draw(text_score);
+    }
+
+}
+
+/**
+ * Method increaseSnake - increases the snake
+ * @return
+ */
+void Game::increaseSnake()
 {
     for (int i = 0; i < field_size_x; i++) {
         for (int j = 0; j < field_size_y; j++) {
@@ -88,8 +206,13 @@ auto increaseSnake(int field[field_size_x][field_size_y])
     }
 }
 
-//Игровая функция по передвижению змейки
-auto movement(int snake_length, int snake_direction, int snake_position_y, int snake_position_x, int field[][field_size_y]) {
+/**
+ * Method movement - responsible for the movement of the snake
+ * Passage through the wall is carried out using: Passage through the wall is carried out by:
+ * zeroing the coordinates or setting the largest coordinate.
+ * sound_ate_apple.play - plays sound when eating an apple.
+ */
+void Game::movement() {
     switch (snake_direction) {
         case snake_direction_up:
             snake_position_y -= 1;
@@ -120,36 +243,34 @@ auto movement(int snake_length, int snake_direction, int snake_position_y, int s
     if ((field[snake_position_x][snake_position_y]) != field_cell_type_none) {
         switch (field[snake_position_x][snake_position_y]) {
             case field_cell_type_apple:
-                increaseSnake(field);
+                sound_ate_apple.play();
+                snake_length += 1;
+                score += 1;
+                if (score % 10 == 0) {
+                    sound_score10.play();
+                }
+                increaseSnake();
+                apple_add();
                 break;
             case field_cell_type_wall:
-                for (int i = 0; i < field_size_x; i++) {
-                    for (int j = 0; j < field_size_y; j++) {
-                        if (field[i][j] > 0) {
-                            field[i][j] = 0;
-                        }
-                    }
-                }
-                return;
+                sound_snake_dead_from_wall.play();
+                game_over = true;
+                break;
             default:
                 if (field[snake_position_x][snake_position_y] > 1) {
-                    for (int i = 0; i < field_size_x; i++) {
-                        for (int j = 0; j < field_size_y; j++) {
-                            if (field[i][j] > 0) {
-                                field[i][j] = 0;
-                            }
-                        }
-                    }
-                    return;
+                    sound_snake_kill_yourself.play();
+                    game_over = true;
                 }
         }
     }
-    for (int i = 0; i < field_size_x; i++) {
-        for (int j = 0; j < field_size_y; j++) {
-            if (field[i][j] > 0) {
-                field[i][j] -= 1;
+    if (!game_over) {
+        for (int i = 0; i < field_size_x; i++) {
+            for (int j = 0; j < field_size_y; j++) {
+                if (field[i][j] > 0) {
+                    field[i][j] -= 1;
+                }
             }
         }
+        field[snake_position_x][snake_position_y] = snake_length;
     }
-    field[snake_position_x][snake_position_y] = snake_length;
 }
