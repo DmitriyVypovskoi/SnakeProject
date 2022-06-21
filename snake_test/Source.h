b1f5fd3bcf7601abf4d0a883f8b1332e07358c81
DmitriyVypovskoi <davypovskoy@miem.hse.ru>
2022-06-21 10:29:41 +0300

    v21

--- snake_test/Source.h
+++ snake_test/Source.h
@@ -20,63 +20,93 @@ sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Snake", sf:
 
 /**
  * Game - initializes variables and methods that are used in the game code
- * field[][] - a two-dimensional array storing the coordinates of the snake.
- * snake_position - the x or y coordinate of the snake.
- * snake length - stores the current length of the snake at the beginning of the game it is equal to 4.
- * score - stores the current account.
- * game paused - pauses the game.
- * game over - a variable that stores the value of losing a game.
  * sf::Texture and sf::Sprite - allows you to initialize images in the game.
  * sf::SoundBuffer and sf::Sound - allows you to use sounds in the game.
  * sf::Font and sf::Text - allows you to use downloaded fonts.
  */
 struct Game
 {
+    /// a two-dimensional array storing the coordinates of the snake.
     int field[field_size_x][field_size_y];
+
+    /// the x coordinate of the snake.
     int snake_position_x = field_size_x / 2;
+
+    /// the y coordinate of the snake.
     int snake_position_y = field_size_y / 2;
+
+    /// stores the current length of the snake at the beginning of the game it is equal to 4.
     int snake_length = 4;
+
+    /// the direction of movement of the snake.
     int snake_direction = snake_direction_right;
+
+    /// stores the current account.
     int score = 0;
 
-
+    /// pauses the game.
     bool game_paused = false;
+
+    /// a variable that stores the value of losing a game.
     bool game_over = false;
 
+    /// snake.png
     sf::Texture snake_picture;
+    /// snake body picture
     sf::Sprite snake;
 
+    /// none.png
     sf::Texture none_picture;
+    /// transparent picture
     sf::Sprite none;
 
+    /// apple.png
     sf::Texture apple_picture;
     sf::Sprite apple;
 
+    /// wall.png
     sf::Texture wall_picture;
+    /// picture of the wall
     sf::Sprite wall;
 
+    /// head.png
     sf::Texture snake_head_picture;
+    /// snake head picture
     sf::Sprite snake_head;
 
+    /// get_apple_2.wav
     sf::SoundBuffer sb_ate_apple;
+    /// sound when eating an apple
     sf::Sound sound_ate_apple;
 
+    /// died.wav
     sf::SoundBuffer sb_snake_kill_yourself;
+    /// sound during death from yourself
     sf::Sound sound_snake_kill_yourself;
 
+    /// pause_game.wav
     sf::SoundBuffer sb_pause_game;
+    /// sound for pause in game
     sf::Sound sound_pause_game;
 
+    /// dead.wav
     sf::SoundBuffer sb_snake_dead_from_wall;
+    /// sound during death from the wall
     sf::Sound sound_snake_dead_from_wall;
 
+    /// score10.wav
     sf::SoundBuffer sb_score10;
+    /// sound at a time when the count is a multiple of 10
     sf::Sound sound_score10;
 
+    ///BigOld.ttf
     sf::Font font_score;
+    /// "Score"
     sf::Text text_score;
 
+    /// BigOld.ttf
     sf::Font font_game_over;
+    /// "Game Over"
     sf::Text text_game_over;
 
     void init_game();
